4a65ff7433c3ba54b7c93265aec668c638dd48ec
Luiz Gustavo <luizgustavo99@live.com>
2022-06-09 17:37:52 -0300

    PCA

--- venv/Jogo.py
+++ venv/Jogo.py
@@ -1,5 +1,7 @@
-import pygame
+import time
+import pygame, sys
 from pygame.locals import *
+from sys import exit
 pygame.init()
 
 x = 125
@@ -10,24 +12,26 @@ velocidade = 10
 white = (255, 255, 255)
 image = pygame.image.load(r'D:\Trabalhos_Unigranrio\PCA\cacapalavras.png')
 image2 = pygame.image.load(r'D:\Trabalhos_Unigranrio\PCA\respostas.png')
+image3 = pygame.image.load(r'D:\Trabalhos_Unigranrio\PCA\descrições.png')
 display_surface = pygame.display.set_mode((800, 800))
-
-
+display_surface2 = pygame.display.set_mode((800, 800))
 pygame.display.set_caption('Caça Palavras')
 
 
 while True:
-    pygame.time.delay(50)
+
 
     #teste utilizando o mouse
     #pygame.mouse.get_pos()
     #posX = pygame.mouse.get_pos()[0]
     #posY = pygame.mouse.get_pos()[1]
     #print(posX, posY)
-
-    display_surface.fill(white)
-    display_surface.blit(image, (x, y))
-    display_surface.blit(image2, (x2, y2))
+    for event in pygame.event.get():
+       if event.type == pygame.MOUSEBUTTONDOWN:
+            pygame.display.init()
+            display_surface.fill(white)
+            display_surface.blit(image, (x, y))
+            display_surface.blit(image2, (x2, y2))
 
     #teste utilizando as setas pra mover a bola
     #comandos = pygame.key.get_pressed()
@@ -42,11 +46,18 @@ while True:
 
 
     #pygame.draw.circle(display_surface, (0, 255, 0), (x, y), 25)
-    pygame.display.update()
+            pygame.display.update()
 
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            display_surface2 = pygame.display.set_mode((800, 800))
+            display_surface2.fill(white)
+            display_surface.blit(image3, (x, y))
+            pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            display_surface = False
+            pygame.quit()
+            sys.exit()
 
 pygame.quit()
